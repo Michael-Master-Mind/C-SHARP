8caba0571c2bf3da6372e64e249ae5a047039a61
Michael Demissew A <michaeldemissew@yahoo.com>
2022-07-05 11:27:06 +0300

    Add item

---
+++ ClassEx2/ClassEx2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassEx2.Iteams;
+namespace ClassEx2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Iteam t = new Iteam();
+            t.Number = textBox1.Text;
+            t.date = dateTimePicker1.Value;
+            t.inventory_number = textBox3.Text;
+            t.object_name = textBox4.Text;
+            t.count =textBox5.Text;
+            t.price = textBox6.Text;
+            t.save();
+            MessageBox.Show("Iteam added successfully!!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ClassEx2/ClassEx2/Iteams/Iteam.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassEx2.Iteams
+{
+    internal class Iteam
+    {
+        public string Number { get; set; }
+        public DateTime date { get; set; }
+        public string inventory_number { get; set; }
+        public string object_name { get; set; }
+        public string count { get; set; }
+        public string price { get; set; }
+
+        public void save() {
+
+            Console.WriteLine(object_name+" iteam added successfully!!");
+        }
+    }
+}
